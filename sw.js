0534916a04d977cab9aad8ae69e937b2ca31f640
Max Alva <malva@peruapps.com.pe>
2020-02-21 10:54:24 -0500

    Cache

--- sw.js
+++ sw.js
@@ -1,3 +1,5 @@
+importScripts('js/sw-utils.js');
+
 const STATIC_CACHE = 'static-v1';
 const DYNAMIC_CACHE = 'dynamic-v1';
 const INMUTABLE_CACHE = 'inmutable-v1';
@@ -24,20 +26,35 @@ const APP_SHELL_INMUTABLE = [
 ];
 
 self.addEventListener( 'install', e => {
-	const staticCache = caches.open(STATIC_CACHE).then(cache => cache.addAll(APP_SHEL));
-	const inmutableCache = caches.open(INMUTABLE_CACHE).then(cache => cache.addAll(APP_SHELL_INMUTABLE));
+	const staticCache = caches.open( STATIC_CACHE ).then( cache => cache.addAll( APP_SHEL ) );
+	const inmutableCache = caches.open( INMUTABLE_CACHE ).then( cache => cache.addAll( APP_SHELL_INMUTABLE ) );
 
-	e.waitUntil(Promise.all([staticCache, inmutableCache]));
+	e.waitUntil( Promise.all( [staticCache, inmutableCache] ) );
 } );
 
-self.addEventListener('activate', e => {
+self.addEventListener( 'activate', e => {
 	const res = caches.keys().then( keys => {
 		keys.forEach( key => {
-			if ( key !== STATIC_CACHE && key.includes('static') ) {
+			if ( key !== STATIC_CACHE && key.includes( 'static' ) ) {
 				return caches.delete( key );
 			}
 		} );
 	} );
 
 	e.waitUntil( res );
-});
+} );
+
+self.addEventListener( 'fetch', e => {
+
+	const res = caches.match( e.request ).then( res => {
+		if ( res ) {
+			return res;
+		}
+
+		return fetch( e.request ).then( newRes => {
+			return updateDynamicCache( DYNAMIC_CACHE, e.request, newRes );
+		} );
+	} );
+
+	e.respondWith( res );
+} );
