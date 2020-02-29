3c7849a5140dbd12c48e4c5be857e0f9717b2a1b
Max Alva <alva85@gmail.com>
2020-02-29 11:42:07 -0500

    fix

--- public/js/camera-class.js
+++ public/js/camera-class.js
@@ -5,38 +5,34 @@ class Camera {
 	}
 
 	openCamera() {
-		if (navigator.mediaDevices) {
-			navigator.mediaDevices.getUserMedia( {
-				audio: false,
-				video: { width: 300, height: 300 }
-			} ).then( stream => {
-				alert('stream');
-				this.videoNode.srcObject = stream;
-				this.stream = stream;
-			} ).catch(e => alert(e));
-		} else {
-			alert('Tu navegador no soporta esta funcionalidad');
-		}
+		navigator.getUserMedia( {
+			audio: false,
+			video: { width: 300, height: 300 }
+		} ).then( stream => {
+			alert( 'stream' );
+			this.videoNode.srcObject = stream;
+			this.stream = stream;
+		} ).catch( e => alert( e ) );
 	}
 
 	closeCamera() {
 		this.videoNode.pause();
 		if ( this.stream ) {
-			this.stream.getTracks().forEach(track => track.stop());
+			this.stream.getTracks().forEach( track => track.stop() );
 		}
 	}
 
 	takePhoto() {
 		// crear elemento canvas para renderizar la foto
-		let canvas = document.createElement('canvas');
+		let canvas = document.createElement( 'canvas' );
 
-		canvas.setAttribute('width', 300);
-		canvas.setAttribute('height', 300);
+		canvas.setAttribute( 'width', 300 );
+		canvas.setAttribute( 'height', 300 );
 
 		// contexto del canvas
-		let context = canvas.getContext('2d');
+		let context = canvas.getContext( '2d' );
 
-		context.drawImage(this.videoNode, 0, 0, canvas.width, canvas.height);
+		context.drawImage( this.videoNode, 0, 0, canvas.width, canvas.height );
 
 		this.photo = context.canvas.toDataURL();
 
